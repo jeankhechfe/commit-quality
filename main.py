5540d60f6552182e914eef182974ba66e529da15
jean khechfe <jeankhechfe@gmail.com>
2020-11-08 20:14:57 +0100

    Remove unused code

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
 import quality
 
-
-# file = open("demo.txt", "r")
-# commitMessages = file.read().split("\n")
-
-file = open("log.txt", "r")
+file = open("commits_logs/linux-insides.txt", "r")
 commits = file.read().split("\ncommit ")
-
-
-# tokens = nltk.word_tokenize(commitMessage)
-# s_tokens = nltk.sent_tokenize(f.read())
-# print(len(commits))
-
-commitMessagesText = commits[1].split("\n")
-# print(quality.get_message_text(commitMessagesText))
-# print(quality.get_quality(commitMessagesText))
-
 
 total_score = 0
 count = 0
@@ -29,10 +15,11 @@ for i in range(1, len(commits)):
     count += 1
     if score in [6, 7]:
         good += 1
-    if score in [4, 5]:
+    elif score in [4, 5]:
         acceptable += 1
-    if score < 4:
+    else:
         bad += 1
+
 print("Commits Quality Average:", "{:.2f}".format(total_score/count), "out of 7")
 print("Good quality:", "{:.2f}".format(good * 100 / count), "%")
 print("Acceptable quality:", "{:.2f}".format(acceptable * 100 / count), "%")
