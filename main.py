64c9e20cc8943f4ea316bead4d9692fde4bb4cc6
jean khechfe <jeankhechfe@gmail.com>
2020-11-08 14:46:26 +0100

    Add qualities percentage

--- main.py
+++ main.py
@@ -17,10 +17,23 @@ commitMessagesText = commits[1].split("\n")
 # print(quality.get_quality(commitMessagesText))
 
 
-total = 0
+total_score = 0
 count = 0
+good = 0
+acceptable = 0
+bad = 0
 for i in range(1, len(commits)):
     commitMessagesText = commits[i].split("\n")
-    total += quality.get_quality(commitMessagesText)
+    score = quality.get_quality(commitMessagesText)
+    total_score += score
     count += 1
-print(total/count)
+    if score in [6, 7]:
+        good += 1
+    if score in [4, 5]:
+        acceptable += 1
+    if score < 4:
+        bad += 1
+print("Commits Quality Average:", "{:.2f}".format(total_score/count), "out of 7")
+print("Good quality:", "{:.2f}".format(good * 100 / count), "%")
+print("Acceptable quality:", "{:.2f}".format(acceptable * 100 / count), "%")
+print("Bad quality:", "{:.2f}".format(bad * 100 / count), "%")
