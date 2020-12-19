77f85196288e24b82a4b0c692a78494a52d19d6b
jean khechfe <jeankhechfe@gmail.com>
2020-12-19 20:24:02 +0100

    Fix commit count

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import quality
 
-file = open("commits_logs/linux-insides.txt", "r")
+file = open("commits_logs/libdc-for-dirk.txt", "r")
 commits = file.read().split("\ncommit ")
 
 total_score = 0
-count = 0
+count = 1
 good = 0
 acceptable = 0
 bad = 0
@@ -20,6 +20,7 @@ for i in range(1, len(commits)):
     else:
         bad += 1
 
+print("Commits Count :", count)
 print("Commits Quality Average:", "{:.2f}".format(total_score/count), "out of 7")
 print("Good quality:", "{:.2f}".format(good * 100 / count), "%")
 print("Acceptable quality:", "{:.2f}".format(acceptable * 100 / count), "%")
