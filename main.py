ad54f11164a4625da45c3aee2502ded6810ca58b
jean khechfe <jeankhechfe@gmail.com>
2020-12-21 15:29:02 +0100

    Get commits from github APIs

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import quality
 import csv
 import textwrap
+import requests
+
 
 file = open("commits_logs/libdc-for-dirk-no-merges.txt", "r")
 commits = file.read().split("\ncommit")
@@ -21,47 +23,53 @@ commits = file.read().split("\ncommit")
 #         txt_file.write(line + '\n')
 
 
-file = open("commits_logs/--.txt", "r")
-commits_dashed = file.read().split('--')
-print(commits_dashed)
+# file = open("commits_logs/--.txt", "r")
+# commits_dashed = file.read().split('\n--')
+#
+# def export_to_csv():
+#     try:
+#         with open('commits.csv', 'w', newline='') as csvfile:
+#             spam_writer = csv.writer(csvfile)
+#             spam_writer.writerow(['number', 'Subject Line', 'Blank Line', 'character count'])
+#             for j in range(1, len(commits_dashed)):
+#                 subject_line = textwrap.dedent(commits_dashed[j].split("\n")[2])
+#                 # separated_with_blank = 1 if textwrap.dedent(commits_dashed[j].split("\n")[2]) == '' else 0
+#
+#                 # spam_writer.writerow([j, subject_line, separated_with_blank, len(subject_line)])
+#                 spam_writer.writerow([j, commits_dashed[j], subject_line, len(subject_line)])
+#
+#     except PermissionError:
+#         print('\x1b[0;30;41m', 'Make sure CSV file is not open', '\x1b[0m')
+#
+# export_to_csv()
+#
+#
+# total_score = 0
+# count = 0
+# good = 0
+# acceptable = 0
+# bad = 0
+# for i in range(1, len(commits)):
+#     commitMessagesText = commits[i].split("\n")
+#     score = quality.get_quality(commitMessagesText)
+#     total_score += score
+#     count += 1
+#     if score in [6, 7]:
+#         good += 1
+#     elif score in [4, 5]:
+#         acceptable += 1
+#     else:
+#         bad += 1
+#
+# print("Commits Count :", count)
+# print("Commits Quality Average:", "{:.2f}".format(total_score/count), "out of 7")
+# print("Good quality:", "{:.2f}".format(good * 100 / count), "%")
+# print("Acceptable quality:", "{:.2f}".format(acceptable * 100 / count), "%")
+# print("Bad quality:", "{:.2f}".format(bad * 100 / count), "%")
 
-def export_to_csv():
-    try:
-        with open('commits.csv', 'w', newline='') as csvfile:
-            spam_writer = csv.writer(csvfile)
-            spam_writer.writerow(['number', 'Subject Line', 'Blank Line', 'character count'])
-            for j in range(1, len(commits_dashed)):
-                subject_line = textwrap.dedent(commits_dashed[j].split("\n")[1])
-                # separated_with_blank = 1 if textwrap.dedent(commits_dashed[j].split("\n")[2]) == '' else 0
-
-                # spam_writer.writerow([j, subject_line, separated_with_blank, len(subject_line)])
-                spam_writer.writerow([j, subject_line, len(subject_line)])
-
-    except PermissionError:
-        print('\x1b[0;30;41m', 'Make sure CSV file is not open', '\x1b[0m')
-
-export_to_csv()
-
-
-total_score = 0
-count = 0
-good = 0
-acceptable = 0
-bad = 0
-for i in range(1, len(commits)):
-    commitMessagesText = commits[i].split("\n")
-    score = quality.get_quality(commitMessagesText)
-    total_score += score
-    count += 1
-    if score in [6, 7]:
-        good += 1
-    elif score in [4, 5]:
-        acceptable += 1
-    else:
-        bad += 1
-
-print("Commits Count :", count)
-print("Commits Quality Average:", "{:.2f}".format(total_score/count), "out of 7")
-print("Good quality:", "{:.2f}".format(good * 100 / count), "%")
-print("Acceptable quality:", "{:.2f}".format(acceptable * 100 / count), "%")
-print("Bad quality:", "{:.2f}".format(bad * 100 / count), "%")
+with open("commits_logs/--new.txt", "w") as txt_file:
+    response = requests.get('https://api.github.com/repos/torvalds/libdc-for-dirk/commits')
+    print(response.json()[0]['commit'])
+    for res in response.json():
+        txt_file.write(res['commit']['message'])
+        txt_file.write('\n----\n')
