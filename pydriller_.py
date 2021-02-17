6a34613005e14c735e80f73991b08eef464ccedf
jean khechfe <jeankhechfe@gmail.com>
2021-02-17 18:32:09 +0100

    Introduce 10 repos to get data from

--- pydriller_.py
+++ pydriller_.py
@@ -1,18 +1,28 @@
 from pydriller import RepositoryMining
 
-repos = ["https://github.com/torvalds/pesconvert",
-         "https://github.com/terryyin/lizard"]
+repos = ["https://github.com/torvalds/subsurface-for-dirk",
+         "https://github.com/bitronix/btm",
+         "https://github.com/EdwardRaff/JSAT",
+         "https://github.com/github/VisualStudio",
+         "https://github.com/jtablesaw/tablesaw",
+         "https://github.com/barryvdh/laravel-debugbar",
+         "https://github.com/terryyin/lizard",
+         "https://github.com/torvalds/uemacs",
+         "https://github.com/apple/swift-llbuild",
+         "https://github.com/docker-library/php"]
 
 with open("commits_logs/--temp.txt", "w") as txt_file:
     for repo in repos:
         i = 0
         commits = RepositoryMining([repo], only_no_merge=True).traverse_commits()
         txt_file.write('\n---project---\n')
-        txt_file.write(next(commits).project_name)
+        txt_file.write(repo)
         for commit in commits:
-            i += 1
-            if i <= 30:  # only first 30 commit (implemented this way cuz pydriller raises exception otherwise)
+            if i < 100:  # only first 100 commit (implemented this way cuz pydriller raises exception otherwise)
+                i += 1
                 txt_file.write('\n---commit---\n')
+                txt_file.write(commit.hash)
+                txt_file.write('\n---message---\n')
                 txt_file.write(commit.msg)
                 txt_file.write('\n---files---\n')
                 editedFilesCount = str(len(commit.modifications))
