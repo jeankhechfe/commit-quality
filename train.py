cdd2fdb346cd99705070ec0facd4abf059e0ad26
jean khechfe <jeankhechfe@gmail.com>
2021-02-21 13:13:27 +0100

    Change training models parameters

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-import joblib
+# import joblib
 
 data = pd.read_csv('data/data.csv')
 x = data.drop(columns=['label'])
@@ -15,7 +15,7 @@ y = data['label']
 def try_model(model):
     accuracy = 0
     for i in range(100):
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
         model.fit(x_train, y_train)
         prediction = model.predict(x_test)
         accuracy += accuracy_score(y_test, prediction)
@@ -25,13 +25,13 @@ def try_model(model):
 m1 = KNeighborsClassifier()
 print("KNeighborsClassifier:", try_model(m1))
 
-m2 = SVC(kernel="linear", C=2)
+m2 = SVC(kernel="linear")
 print("SVC:", try_model(m2))
 
 m3 = DecisionTreeClassifier()
 print("DecisionTreeClassifier:", try_model(m3))
 
-m4 = RandomForestClassifier(n_estimators=60, max_depth=3, random_state=1)
+m4 = RandomForestClassifier()  # n_estimators=100, max_depth=10, random_state=1
 print("RandomForestClassifier:", try_model(m4))
 
 # for i in range(len(prediction)):
