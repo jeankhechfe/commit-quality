a827ca418829218c4d52404a29c81ec9db05298f
jean khechfe <jeankhechfe@gmail.com>
2021-03-05 19:34:32 +0100

    Add F1 score to the metrics

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
@@ -7,27 +8,34 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
 import timeit
 
 
 data = pd.read_csv('data/data.csv')
 x = data.drop(columns=['label'])
+# x = data.drop(columns=['changed_files_count', 'changes_methods_count', 'files_to_body_ratio',
+#                        'methods_to_body_ratio', 'methods_long', 'methods_complexity',
+#                        'methods_parameters', 'added_lines', 'removed_lines', 'label'])
 y = data['label']
 
 
 def try_model(model):
     accuracies = []
+    f1_scores = []
     start = timeit.default_timer()
     for i in range(100):
         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
         model.fit(x_train, y_train)
         prediction = model.predict(x_test)
         accuracies.append(accuracy_score(y_test, prediction))
+        f1_scores.append(f1_score(y_test, prediction, average=None, labels=['good', 'neutral', 'bad']))
     stop = timeit.default_timer()
-    print("{}: [highest {}] [accuracy {}] [time {}]".format(
+    print("{}: [highest {}] [accuracy {}] [f1 {}] [time {}]".format(
         model.__class__.__name__,
         round(max(accuracies), 2),
         round(sum(accuracies) / len(accuracies), 2),
+        np.round(sum(f1_scores) / len(f1_scores), 2),
         round(stop - start, 2)))
 
 
